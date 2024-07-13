49a862a16198160a925500ef704d3633898ad019
uhidontkno <hi@rare1k.dev>
2024-07-13 12:27:35 -0400

    feat: add auth inputs

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -10,8 +10,13 @@
 </script>
 <dialog id="authModal" class="modal backdrop-blur modal-bottom md:modal-middle">
   <div class="modal-box">
-    <h3 class="text-lg font-bold">Hello!</h3>
-    <p class="py-4">Press ESC key or click the button below to close</p>
+    <h3 class="text-lg font-bold">Welcome back!</h3>
+    <p class="py-4">
+        <label for="username">Username:</label>
+        <input name="username" type="username" placeholder="johndoe123" class="input input-bordered !border-none w-full max-w-xs" />
+        <label for="password">Password:</label>
+        <input name="password" type="password" placeholder="Shh! It's a secret!" class="input input-bordered !border-none w-full max-w-xs" />
+    </p>
     <div class="modal-action">
       <!-- <form method="dialog">-->
         <a href="?" class="btn btn-primary !px-6">Login</a>
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -11,7 +11,14 @@
 <dialog id="authModal" class="modal backdrop-blur modal-bottom md:modal-middle">
   <div class="modal-box">
     <h3 class="text-lg font-bold">Hello!</h3>
-    <p class="py-4">Press ESC key or click the button below to close</p>
+    <p class="py-4">
+        <label for="email">Email:</label>
+        <input name="email" type="email" placeholder="wedontsendyouspam@rare1k.dev" class="input input-bordered !border-none w-full max-w-xs" />
+        <label for="username">Username:</label>
+        <input name="username" type="username" placeholder="johndoe123" class="input input-bordered !border-none w-full max-w-xs" />
+        <label for="password">Password:</label>
+        <input name="password" type="password" placeholder="Shh! It's a secret!" class="input input-bordered !border-none w-full max-w-xs" />
+    </p>
     <div class="modal-action">
       <!-- <form method="dialog">-->
         <a href="?" class="btn btn-primary !px-6">Signup</a>

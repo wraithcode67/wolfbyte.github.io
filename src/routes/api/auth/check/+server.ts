ff974d41cab4f6df6bb0794d274973a151933153
uhidontkno <hi@rare1k.dev>
2024-07-14 17:28:55 -0400

    feat: add validation for jwt tokens

---
+++ src/routes/api/auth/check/+server.ts
@@ -0,0 +1,24 @@
+import { text } from '@sveltejs/kit';
+import mac from "node-macaddress";
+let secretKey = btoa(mac.one());
+import jwt from "jsonwebtoken";
+export async function POST({ request, cookies }) {
+    const token = request.body.token; // Get the JWT token from the request body
+  
+    if (!token) {
+      return text('false');
+    }
+  
+    try {
+        if (token && jwt.decode(token)) {
+            // @ts-expect-error
+            const expiry = jwt.decode(token).exp;
+            const now = new Date();
+            return text(String(now.getTime() > expiry * 1000));
+          }
+          return text(String(false));
+    } catch (error) {
+      return text('false');
+    }
+    return text('true');
+  }

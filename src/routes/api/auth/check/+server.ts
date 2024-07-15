30b713b05494090ff28f64e3f483c89774694502
uhidontkno <hi@rare1k.dev>
2024-07-15 10:44:55 -0400

    fix: fixes

--- src/routes/api/auth/check/+server.ts
+++ src/routes/api/auth/check/+server.ts
@@ -1,9 +1,11 @@
 import { text } from '@sveltejs/kit';
+// @ts-expect-error
 import mac from "node-macaddress";
 let secretKey = btoa(mac.one());
 import jwt from "jsonwebtoken";
 export async function POST({ request, cookies }) {
-    const token = request.body.token; // Get the JWT token from the request body
+
+    const token = await request.text(); // Get the JWT token from the request body
   
     if (!token) {
       return text('false');
@@ -13,6 +15,7 @@ export async function POST({ request, cookies }) {
         if (token && jwt.decode(token)) {
             // @ts-expect-error
             const expiry = jwt.decode(token).exp;
+            if (!expiry) {return text("false")}
             const now = new Date();
             return text(String(now.getTime() > expiry * 1000));
           }

a403e3132162b20293cede96294a16e7e7e7219c
uhidontkno <hi@rare1k.dev>
2024-07-16 13:41:35 -0400

    feat: i forgor

--- src/routes/app/dash/admin/+page.svelte
+++ src/routes/app/dash/admin/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 import { validateAuth } from "$lib/client";import { onMount } from "svelte";onMount((async()=>{
     await validateAuth();
-    let r = await fetch("/api/auth/isUserAdmin",{method:"POST",body:localStorage["__jwt_user"]})
-    if (await r.text() != "true") {
+    let r = await fetch("/api/auth/getSettings",{method:"POST",body:JSON.stringify({token:localStorage["__jwt_auth"],username:localStorage["__jwt_user"]})})
+    let rj = await r.json();
+    if (!rj.hasAdmin) {
         document.location = "/app"
     }
     }));

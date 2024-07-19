577b479d006704dea01ce3aee7c3a1e04be09fc0
uhidontkno <hi@rare1k.dev>
2024-07-19 19:18:25 -0400

    feat: start working on runner add/remove

---
+++ src/routes/api/admin/runner/add/+server.ts
@@ -0,0 +1,18 @@
+import { text,json } from '@sveltejs/kit';
+// @ts-expect-error
+import mac from "node-macaddress";
+let secretKey = btoa(mac.one());
+import { db } from '$lib/db';
+import jwt, { type JwtPayload } from "jsonwebtoken";
+export async function POST({ request, cookies }) {
+    let { token, runnerUrl } = await request.json();
+    if (token && jwt.decode(token)) {
+        let d: string | JwtPayload = jwt.verify(token,secretKey,undefined)
+        if (!d || typeof d == "string" ) {return json({"error":"Invalid JWT token"},{"status":400})}
+        if (!d.exp || Date.now() > d.exp * 1000) {return json({"error":"Invalid JWT token"},{"status":400})}
+        let jwtAdmin = (await db.settings.findFirst({where: {user: {username: d.username }}}))?.hasAdmin
+        if (!jwtAdmin) {
+            return json({"error":"You do not have permission to do this."},{"status":403})
+        }
+    }
+}

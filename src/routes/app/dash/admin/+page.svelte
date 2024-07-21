a71f35bde8497f7990681ab5730d4ef5fb9db474
uhidontkno <hi@rare1k.dev>
2024-07-21 14:39:11 -0400

    feat: fix styles

--- src/routes/app/dash/admin/+page.svelte
+++ src/routes/app/dash/admin/+page.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
 import { UserX,Crown,Gem,UserCog,Trash2 } from "lucide-svelte";
-import { validateAuth } from "$lib/client";import { onMount } from "svelte";onMount((async()=>{
+import { validateAuth } from "$lib/client";import { onMount } from "svelte";
+onMount((async()=>{
     await validateAuth();
     let r = await fetch("/api/auth/getSettings",{method:"POST",body:JSON.stringify({token:localStorage["__jwt_auth"],username:localStorage["__jwt_user"]})})
     let rj = await r.json();
     if (!rj.hasAdmin) {
         document.location = "/app"
     }
-    }));
+    console.log("hi")
+}));
 </script>
-<div class="dashboard">
+<div class="dashboard ml-2 flex flex-col">
   <h1 class="font-bold text-3xl my-3">Admin Dashboard</h1>
   <h2 class="font-bold text-accent text-2xl my-3 ">Instance Settings</h2>
-  <div class="p-3 bg-base-200 rounded-xl">
-    <span class="flex flex-row items-center align-middle mb-5">
+  <div class="p-3 bg-base-200 rounded-xl w-fit max-w-fit">
+    <span class="flex flex-row items-center align-middle mb-5 w-fit max-w-fit">
       Allow Signups: <input type="checkbox" class="toggle toggle-success ml-4" checked>
     </span>
     <span class="catwayRunners">
       <b class="text-lg">Catway Runners</b>
       <div class="p-6 bg-base-300 rounded-xl">
-        <pre>No Runners found.</pre>
+        <pre class="runnersList">No Runners found.</pre>
         <br>
         <span class="flex flex-row items-center align-middle gap-3">
           <button class="btn btn-sm btn-success">Add</button> <button class="btn btn-sm btn-error">Delete</button>
@@ -28,9 +30,9 @@ import { validateAuth } from "$lib/client";import { onMount } from "svelte";onMo
     </span>
   </div>
   <h2 class="font-bold text-accent text-2xl my-3">User Management</h2>
-  <div class="p-3 bg-base-200 rounded-xl min-h-[240px] max-h-[480px] min-w-[50%]">
+  <div class="p-3 bg-base-200 rounded-xl min-h-[240px] max-h-[480px] min-w-[50%] max-w-[640px]">
     <button class="btn btn-sm btn-success mb-2">Add</button>
-    <div class=" template">
+    <div class="uCard template">
       <span class="badges flex flex-row gap-1">
         <span title="This user is an admin." class="adminIcon"><Crown class="text-warning m-0 p-0" size="20"/></span>
         <span title="This user has Catway Purrmium." class="premiumIcon"><Gem class="text-primary m-0 p-0" size="20" /></span>
@@ -43,7 +45,7 @@ import { validateAuth } from "$lib/client";import { onMount } from "svelte";onMo
     </div>
   </div>
   <h2 class="font-bold text-accent text-2xl my-3">Session Management</h2>
-  <div class="p-6 bg-base-200 rounded-xl min-h-[240px] max-h-[480px] min-w-[50%]">
+  <div class="p-6 bg-base-200 rounded-xl min-h-[240px] max-h-[480px] min-w-[50%] max-w-[640px]">
     <div class="pl-3  flex flex-row align-middle items-center gap-4 template">
       <b>Name</b><b>Owned By</b>
     </div>

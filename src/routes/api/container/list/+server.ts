363f2089488071c1efbd4ec89cb963569f524e37
uhidontkno <hi@rare1k.dev>
2024-07-31 15:47:03 -0400

    feat: make container listing not an admin-only endpoint

--- src/routes/api/container/list/+server.ts
+++ src/routes/api/container/list/+server.ts
@@ -10,10 +10,6 @@ export async function POST({ request, cookies }) {
         let d: string | JwtPayload = jwt.verify(token,secretKey,undefined)
         if (!d || typeof d == "string" ) {return json({"error":"Invalid JWT token"},{"status":400})}
         if (!d.exp || Date.now() > d.exp * 1000) {return json({"error":"Invalid JWT token"},{"status":400})}
-        let jwtAdmin = (await db.settings.findFirst({where: {user: {username: d.username }}}))?.hasAdmin
-        if (!jwtAdmin) {
-            return json({"error":"You do not have permission to do this."},{"status":403})
-        }
     } else {
         return json({"error":"No JWT token provided or it is invalid"},{"status":400})
     }

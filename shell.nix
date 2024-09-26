74283da6a17e1af484a64ed60d747d3eab370346
rare1k <hi@rare1k.dev>
2024-09-26 12:14:25 -0400

    feat: working prisma on nixos

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{pkgs ? import <nixpkgs> {}}: let
+  lib = pkgs.lib;
+in
+  pkgs.mkShell (with pkgs; {
+    buildInputs = [
+      nodejs
+      openssl
+    ];
+
+    shellHook = ''PATH="$PWD/node_modules/.bin:$PATH"'';
+
+    PRISMA_SCHEMA_ENGINE_BINARY = "${prisma-engines}/bin/schema-engine";
+    PRISMA_QUERY_ENGINE_BINARY = "${prisma-engines}/bin/query-engine";
+    PRISMA_QUERY_ENGINE_LIBRARY = "${prisma-engines}/lib/libquery_engine.node";
+    PRISMA_INTROSPECTION_ENGINE_BINARY = "${prisma-engines}/bin/introspection-engine";
+    PRISMA_FMT_BINARY = "${prisma-engines}/bin/prisma-fmt";
+  })
